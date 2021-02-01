fb7e64408259ff1ee014f56d29fee4db81c7df24
leny <info@flatland.be>
2021-02-01 23:20:38 +0100

    Prepare useGMapAPILoader hook

---
+++ src/core/hooks/use-gmap-api-loader.js
@@ -0,0 +1,26 @@
+/* leny/enigjewo
+ *
+ * /src/core/hooks/use-gmap-api-loader.js - Hook: useGMapAPILoader
+ *
+ * coded by leny@BeCode
+ * started at 01/02/2021
+ */
+
+import {useState} from "react";
+import {Loader} from "@googlemaps/js-api-loader";
+
+import {GMAP_API_KEY} from "core/constants";
+
+export const useGMapAPILoader = (options = {}) => {
+    const [ready, setReady] = useState(false);
+
+    const loader = new Loader({
+        apiKey: GMAP_API_KEY,
+        version: "weekly",
+        ...options,
+    });
+
+    !ready && loader.load().then(() => setReady(true));
+
+    return [!ready, ready]; // loading, ready
+};
