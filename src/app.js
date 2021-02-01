90f454ea3cd063657cff517502b1bbd874800fe4
leny <info@flatland.be>
2021-02-01 15:53:16 +0100

    Init app with basic react structure

---
+++ src/app.js
@@ -0,0 +1,17 @@
+/* leny/enigjewo
+ *
+ * /src/app.js - Main entry point
+ *
+ * coded by leny@BeCode
+ * started at 01/02/2021
+ */
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Hello from "./components/hello";
+
+ReactDOM.render(
+    <Hello />,
+    document.querySelector("#app"),
+);
---
+++ src/components/hello.js
@@ -0,0 +1,13 @@
+/* leny/enigjewo
+ *
+ * /src/components/hello.js - Dumb first component
+ *
+ * coded by leny@BeCode
+ * started at 01/02/2021
+ */
+
+import React from "react";
+
+const Hello = () => <p>{"Hello, world!"}</p>;
+
+export default Hello;
