7e80be85a186a7e1ca68fcfa658e97ee7a233d7d
leny <info@flatland.be>
2021-02-01 22:45:43 +0100

    Add eslint-plugin-react-hooks & use new react automatic runtime

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,23 @@
+/* leny/enigjewo
+ *
+ * /.eslintrc.js - ESLint config
+ *
+ * coded by leny@BeCode
+ * started at 01/02/2021
+ */
+
+const OFF = 0;
+const WARNING = 1;
+const ERROR = 2;
+
 module.exports = {
     extends: "@becode",
+    plugins: ["react-hooks"],
+    rules: {
+        "react-hooks/rules-of-hooks": ERROR,
+        "react-hooks/exhaustive-deps": WARNING,
+        // cf. https://reactjs.org/blog/2020/09/22/introducing-the-new-jsx-transform.html
+        "react/jsx-uses-react": OFF,
+        "react/react-in-jsx-scope": OFF,
+    },
 };
