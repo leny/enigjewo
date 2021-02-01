2e2596696285eb5d7cb9ad1d530fa01c7a11372c
leny <info@flatland.be>
2021-02-01 23:39:03 +0100

    Show loading animation while loading GMap API

--- src/containers/root.js
+++ src/containers/root.js
@@ -7,13 +7,28 @@
  */
 
 import {useState} from "react";
+import {useGMapAPILoader} from "hooks/use-gmap-api-loader";
+import classnames from "classnames";
 
 import {MODE_MENU, MODE_GAME} from "core/constants";
 
 import MenuContainer from "containers/menu";
 
+import Loading from "components/commons/loading";
+
 const RootContainer = () => {
+    const [loading] = useGMapAPILoader();
     const [mode, setMode] = useState(MODE_MENU);
+
+    if (loading) {
+        return (
+            <section className={"section"}>
+                <div className={classnames("container", "has-text-centered")}>
+                    <Loading variant={"info"} size={"large"} />
+                </div>
+            </section>
+        );
+    }
 
     if (mode === MODE_GAME) {
         console.log("Show game window!");
