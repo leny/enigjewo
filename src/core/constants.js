ef24958019379b00023ac8965d9bf3954efd5765
leny <info@flatland.be>
2021-02-01 21:47:00 +0100

    Prepare constants file

---
+++ src/core/constants.js
@@ -0,0 +1,10 @@
+/* leny/enigjewo
+ *
+ * /src/core/constants.js - Constants
+ *
+ * coded by leny@BeCode
+ * started at 01/02/2021
+ */
+
+export const MODE_MENU = "menu";
+export const MODE_GAME = "game";
