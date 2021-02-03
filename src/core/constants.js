ed97e7c1d164925b3eaddcf9857e19c8d55fcfba
leny <info@flatland.be>
2021-02-04 00:24:55 +0100

    Add default difficulty constant

--- src/core/constants.js
+++ src/core/constants.js
@@ -14,6 +14,7 @@ export const MODE_GAME = "game";
 export const GMAP_API_KEY = process.env.GMAP_API_KEY;
 
 export const DEFAULT_ROUND_DURATION = 300; // five minutes
+export const DEFAULT_DIFFICULTY = 2000;
 
 export const BSP = "\u0020";
 export const NBSP = "\u00a0";
