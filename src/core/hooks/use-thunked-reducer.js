3f81b6d44ade54dcb7766b0b5f427d3ec4f459a4
leny <info@flatland.be>
2021-02-03 21:45:08 +0100

    Implement useThunkedReducer hook

---
+++ src/core/hooks/use-thunked-reducer.js
@@ -0,0 +1,25 @@
+/* leny/enigjewo
+ *
+ * /src/core/hooks/use-thunked-reducer.js - Custom hook: useThunkedReducer
+ *
+ * coded by leny@BeCode
+ * started at 03/02/2021
+ */
+
+import {useReducer, useMemo} from "react";
+
+export const useThunkedReducer = (...props) => {
+    const [state, dispatch] = useReducer(...props);
+    const thunkedDispatch = useMemo(
+        () => action => {
+            if (typeof action === "function") {
+                action(dispatch);
+            } else {
+                dispatch(action);
+            }
+        },
+        [dispatch],
+    );
+
+    return [state, thunkedDispatch];
+};
