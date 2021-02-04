5164310c5dc571945b86cb752b159f8cef569b04
leny <info@flatland.be>
2021-02-04 15:26:03 +0100

    Extract common StreetView component

---
+++ src/components/commons/street-view.js
@@ -0,0 +1,60 @@
+/* leny/enigjewo
+ *
+ * /src/components/commons/street-view.js - Common Component: Google StreetView
+ *
+ * coded by leny@BeCode
+ * started at 04/02/2021
+ */
+
+/* global google */
+
+import "styles/game.scss";
+
+import {useRef, useState, useEffect} from "react";
+
+import classnames from "classnames";
+import PropTypes from "prop-types";
+
+const StreetView = ({className, panorama, discriminator = 0}) => {
+    const box = useRef(null);
+    const [streetView, setStreetView] = useState(null);
+
+    useEffect(() => {
+        if (streetView || !box.current) {
+            return;
+        }
+
+        setStreetView(
+            new google.maps.StreetViewPanorama(box.current, {
+                addressControl: false,
+                fullscreenControl: false,
+                motionTracking: false,
+                motionTrackingControl: false,
+                showRoadLabels: false,
+                panControl: true,
+            }),
+        );
+    }, [box, streetView, setStreetView]);
+
+    useEffect(() => {
+        if (!streetView || !panorama) {
+            return;
+        }
+
+        streetView.setPano(panorama);
+        streetView.setPov({
+            heading: 270,
+            pitch: 0,
+        });
+        streetView.setZoom(0);
+    }, [streetView, panorama, discriminator]);
+
+    return <div className={classnames(className, "expand")} ref={box} />;
+};
+
+StreetView.propTypes = {
+    panorama: PropTypes.string.isRequired,
+    discriminator: PropTypes.number,
+};
+
+export default StreetView;
--- src/components/game/panorama.js
+++ src/components/game/panorama.js
@@ -6,54 +6,21 @@
  * started at 02/02/2021
  */
 
-/* global google */
-
 import "styles/game/panorama.scss";
 
-import {useRef, useState, useEffect, useContext} from "react";
+import PropTypes from "prop-types";
+
+import {useContext} from "react";
 import {GameStoreContext} from "store/game";
 
-import classnames from "classnames";
-import PropTypes from "prop-types";
+import StreetView from "components/commons/street-view";
 
 const Panorama = ({discriminator}) => {
     const {
         currentRound: {panorama},
     } = useContext(GameStoreContext);
-    const box = useRef(null);
-    const [streetView, setStreetView] = useState(null);
 
-    useEffect(() => {
-        if (streetView || !box.current) {
-            return;
-        }
-
-        setStreetView(
-            new google.maps.StreetViewPanorama(box.current, {
-                addressControl: false,
-                fullscreenControl: false,
-                motionTracking: false,
-                motionTrackingControl: false,
-                showRoadLabels: false,
-                panControl: true,
-            }),
-        );
-    }, [box, streetView, setStreetView]);
-
-    useEffect(() => {
-        if (!streetView || !panorama) {
-            return;
-        }
-
-        streetView.setPano(panorama);
-        streetView.setPov({
-            heading: 270,
-            pitch: 0,
-        });
-        streetView.setZoom(0);
-    }, [streetView, panorama, discriminator]);
-
-    return <div className={classnames("panorama")} ref={box} />;
+    return <StreetView panorama={panorama} discriminator={discriminator} />;
 };
 
 Panorama.propTypes = {
