157fa8b9d27a24cc433693697d3ba9af3d546967
leny <info@flatland.be>
2021-02-04 22:17:43 +0100

    Prepare marker icons

---
+++ src/core/icons.js
@@ -0,0 +1,25 @@
+/* leny/enigjewo
+ *
+ * /src/core/icons.js - Icon utils
+ *
+ * coded by leny@BeCode
+ * started at 04/02/2021
+ */
+
+/* global google */
+
+import CorrectPositionIcon from "icons/correct-position.png";
+import Player1Icon from "icons/player-1.png";
+
+const icons = {
+    target: CorrectPositionIcon,
+    player1: Player1Icon,
+};
+
+export const getMarkerIcon = icon => ({
+    url: icons[icon],
+    size: new google.maps.Size(24, 24),
+    scaledSize: new google.maps.Size(24, 24),
+    origin: new google.maps.Point(0, 0),
+    anchor: new google.maps.Point(12, 12),
+});
