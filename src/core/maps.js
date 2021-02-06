9e9c7927798c1c1820a7ee01ab0a92ea0b6d80fe
leny <info@flatland.be>
2021-02-07 00:18:48 +0100

    Implement loading map mecanism

--- src/core/maps.js
+++ src/core/maps.js
@@ -6,7 +6,11 @@
  * started at 04/02/2021
  */
 
-import Europe from "url:../data/maps/europe.geojson";
+import axios from "axios";
+
+import europe from "url:../data/maps/europe.geojson";
+import belgium from "url:../data/maps/belgium.geojson";
+import france from "url:../data/maps/france.geojson";
 
 export const maps = {
     world: {
@@ -15,6 +19,27 @@ export const maps = {
     },
     europe: {
         label: "🇪🇺 Europe (without Russia)",
-        data: Europe,
+        data: europe,
     },
+    belgium: {
+        label: "🇧🇪 Belgium",
+        data: belgium,
+    },
+    france: {
+        label: "🇫🇷 France",
+        data: france,
+    },
+};
+
+const cache = new Map();
+
+export const loadGeoJSON = async map => {
+    if (cache.has(map)) {
+        return cache.get(map);
+    }
+    const {data: geoJSON} = await axios.get(maps[map].data, {
+        responseType: "json",
+    });
+    cache.set(map, geoJSON);
+    return geoJSON;
 };
