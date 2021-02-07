78566cb1b2ede16143bd32d483898ab4acf3ce21
leny <info@flatland.be>
2021-02-08 00:12:40 +0100

    (WIP) Refactor initState of Game store

--- src/store/game/index.js
+++ src/store/game/index.js
@@ -6,16 +6,13 @@
  * started at 03/02/2021
  */
 
-import {
-    DEBUG,
-    DEFAULT_ROUND_DURATION,
-    DEFAULT_DIFFICULTY,
-} from "core/constants";
+import {DEBUG} from "core/constants";
 import {
     STEP_LOADING,
     STEP_PLAY,
     STEP_RESULTS,
     STEP_SUMMARY,
+    ACTION_PREPARE_GAME,
     ACTION_PREPARE_ROUND,
     ACTION_START_ROUND,
     ACTION_PREPARE_RESULTS,
@@ -28,29 +25,48 @@ import {createContext} from "react";
 
 export const GameStoreContext = createContext();
 
-export const initState = ({
-    totalRounds = 5,
-    roundDuration = DEFAULT_ROUND_DURATION,
-    map = "world",
-}) => ({
-    map,
-    difficulty: DEFAULT_DIFFICULTY,
-    bounds: null,
-    rounds: {
-        total: totalRounds,
-        current: 0,
-        duration: roundDuration === 0 ? false : roundDuration,
+export const initState = () => ({
+    code: null, // (multi) String: hash code to join game
+    title: null, // (multi) String: title of the game
+    settings: {
+        // rounds Number
+        // duration Number: round duration - in seconds (false for infinite)
+        // isMulti Bool: is multiplayer game
+        // map String
+        // difficulty Number
+        // bounds (local) Object - computed from map, cached in the store
     },
-    currentRound: null /* {
+    player: null, // String: id of the current player
+    players: {
+        /*[player_id]: {
+            name String
+            isOwner (multi) Bool: is owner of the game - will run actions to initiate rounds
+            score Number: computed from entries
+        }*/
+    },
+    rounds: {
+        // one entry per round
+        /*["rnd-X"] Object {
+            panorama String: panorama id from StreetView
+            target Object: latLng from StreetView
+        }*/
+    },
+    entries: {
+        // one entry per round per player
+        /*{
+            ["rnd-X-PLAYER_KEY"] Object {
+                position Object: latLng of player's guess for the round
+                distance Number: in meters
+                score Number
+                startedAt Number: timestamp
+                endedAt Number: timestamp
+            }
+        }*/
+    },
+    currentRound: {
         index: 0,
-        panorama: null,
-        score: 0,
-    } */,
-    panoramas: [],
-    targets: [],
-    positions: [],
-    distances: [],
-    scores: [],
+        // startedAt (multi) Number: reference of start - for tempo & waiting for players
+    },
     step: STEP_LOADING,
     ended: false,
 });
@@ -59,63 +75,141 @@ export const reducer = (state, {type, ...payload}) => {
     DEBUG && console.log("DEBUG:reducer:", {type, payload});
 
     switch (type) {
-        case ACTION_PREPARE_ROUND:
-            return {
-                ...state,
-                rounds: {
-                    ...state.rounds,
-                    current: state.rounds.current + 1,
-                },
-                currentRound: null,
-                step: STEP_LOADING,
-            };
-        case ACTION_START_ROUND: {
-            const {panorama, target, difficulty, bounds} = payload;
+        case ACTION_PREPARE_GAME: {
+            const {
+                code,
+                title,
+                rounds,
+                duration,
+                map,
+                isMulti,
+                player: {key, name, isOwner},
+            } = payload;
 
             return {
                 ...state,
-                difficulty,
-                bounds,
-                panoramas: [...state.panoramas, panorama],
-                targets: [...state.targets, target],
+                code,
+                title,
+                settings: {
+                    rounds,
+                    duration,
+                    isMulti,
+                    map,
+                },
+                player: key,
+                players: {
+                    [key]: {
+                        name,
+                        isOwner,
+                        score: 0,
+                    },
+                },
+                step: STEP_LOADING,
+            };
+        }
+        case ACTION_PREPARE_ROUND:
+            return {
+                ...state,
                 currentRound: {
-                    index: state.rounds.current,
-                    panorama,
-                    score: state.scores.reduce((acc, elt) => acc + elt, 0),
+                    index: state.currentRound.index + 1,
+                },
+                step: STEP_LOADING,
+            };
+        case ACTION_START_ROUND: {
+            const {panorama, target, difficulty, bounds, now} = payload;
+            const key = `rnd-${state.currentRound.index}-${state.player}`;
+
+            return {
+                ...state,
+                settings: {
+                    ...state.settings,
+                    difficulty,
+                    bounds,
+                },
+                rounds: {
+                    ...state.rounds,
+                    [`rnd-${state.currentRound.index}`]: {
+                        panorama,
+                        target,
+                    },
+                },
+                entries: {
+                    ...state.entries,
+                    [key]: {
+                        startedAt: now,
+                    },
+                },
+                currentRound: {
+                    ...state.currentRound,
+                    startedAt: now,
                 },
                 step: STEP_PLAY,
             };
         }
-        case ACTION_PREPARE_RESULTS:
-            return {
-                ...state,
-                step: STEP_LOADING,
-            };
-        case ACTION_COMPUTE_RESULTS: {
-            const {position} = payload;
+        case ACTION_PREPARE_RESULTS: {
+            const {now} = payload;
+            const key = `rnd-${state.currentRound.index}-${state.player}`;
 
             return {
                 ...state,
-                positions: [...state.positions, position],
+                entries: {
+                    ...state.entries,
+                    [key]: {
+                        ...state.entries[key],
+                        endedAt: now,
+                    },
+                },
+                step: STEP_LOADING,
+            };
+        }
+        case ACTION_COMPUTE_RESULTS: {
+            const {position} = payload;
+            const key = `rnd-${state.currentRound.index}-${state.player}`;
+
+            return {
+                ...state,
+                entries: {
+                    ...state.entries,
+                    [key]: {
+                        ...state.entries[key],
+                        position,
+                    },
+                },
                 step: STEP_LOADING,
             };
         }
         case ACTION_SHOW_RESULTS: {
             const {distance, score} = payload;
+            const key = `rnd-${state.currentRound.index}-${state.player}`;
 
             return {
                 ...state,
-                scores: [...state.scores, score],
-                distances: [...state.distances, distance],
-                currentRound: {
-                    ...state.currentRound,
-                    score: [...state.scores, score].reduce(
-                        (acc, elt) => acc + elt,
-                        0,
-                    ),
+                players: {
+                    ...state.players,
+                    [state.player]: {
+                        ...state.player,
+                        score: Array.from(
+                            new Array(state.currentRound.index - 1).keys(),
+                            i => i + 1,
+                        ).reduce(
+                            (acc, ind) =>
+                                acc +
+                                state.entries[`rnd-${ind}-${state.player}`]
+                                    .score,
+                            score,
+                        ),
+                    },
+                },
+                entries: {
+                    ...state.entries,
+                    [key]: {
+                        ...state.entries[key],
+                        distance,
+                        score,
+                    },
                 },
                 step: STEP_RESULTS,
-                ended: state.rounds.current === state.rounds.total,
+                ended: state.currentRound.index === state.settings.rounds,
             };
         }
         case ACTION_SHOW_SUMMARY:
