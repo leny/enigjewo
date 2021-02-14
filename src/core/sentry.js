4ae88b3f0ae8bdbc642e0966ef7f4ed4ce7ca252
leny <info@flatland.be>
2021-02-14 23:31:55 +0100

    Fix Sentry config

--- src/core/sentry.js
+++ src/core/sentry.js
@@ -8,12 +8,9 @@
 
 import {SENTRY_DSN, USE_SENTRY} from "core/constants";
 import * as Sentry from "@sentry/react";
-import {Integrations} from "@sentry/tracing";
 
 if (USE_SENTRY) {
     Sentry.init({
         dsn: SENTRY_DSN,
-        integrations: [new Integrations.BrowserTracing()],
-        tracesSampleRate: 0.1,
     });
 }
