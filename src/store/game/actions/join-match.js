b1ea9a9aa10af95ff2896c48fcebb548ecbca1dd
leny <info@flatland.be>
2022-08-07 17:36:07 +0200

    Avoid joining a challenge twice

--- src/store/game/actions/join-match.js
+++ src/store/game/actions/join-match.js
@@ -6,7 +6,12 @@
  * started at 09/02/2021
  */
 
-import {ACTION_JOIN_GAME, ACTION_SEND_PLAYER_INFOS} from "store/game/types";
+import {
+    ACTION_JOIN_GAME,
+    ACTION_SEND_PLAYER_INFOS,
+    ACTION_INJECT_SUMMARY,
+    ACTION_SHOW_SUMMARY,
+} from "store/game/types";
 import {db} from "core/firebase";
 
 export default settings => async dispatch => {
@@ -16,6 +21,15 @@ export default settings => async dispatch => {
         code,
         player: {key, icon, name, isOwner},
     } = settings;
+
+    const players = (await db.ref(`games/${code}/players`).get()).val();
+
+    if (Object.values(players).find(player => player.name === name)) {
+        const endedGame = (await db.ref(`games/${code}`).get()).val();
+        dispatch({type: ACTION_INJECT_SUMMARY, ...endedGame});
+        dispatch({type: ACTION_SHOW_SUMMARY});
+        return;
+    }
 
     await db.ref(`games/${code}/players`).update({
         [key]: {icon, name, isOwner, isActive: true},
